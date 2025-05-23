fa8c4421573f8071e59dea9b259eaade014f58c5
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2025-05-23 16:37:05 -0300

    feat: devenv environment setup

---
+++ devenv.nix
@@ -0,0 +1,51 @@
+{ pkgs, ... }:
+
+{
+  env.GREET = "devenv";
+  env.DATABASE_URL = "sqlite://dev.db";
+  env.RUST_LOG = "debug";
+  cachix.enable = false;
+
+  packages = with pkgs; [
+    git
+    cargo
+    sqlite
+    sqlx-cli
+
+    # IDE
+    rust-analyzer
+    rustfmt
+    clippy
+    vscode-extensions.vadimcn.vscode-lldb.adapter # For DAP
+  ];
+
+  languages.rust.enable = true;
+
+  processes.cargo-watch.exec = "cargo-watch";
+
+  # services.postgresql.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.migrate.exec = ''
+    echo "Running migrations..."
+    output=$(cargo sqlx migrate run 2>&1)
+    if [ -z "$output" ]; then
+      echo "No migrations to run."
+    else
+      echo "$output"
+      echo "Migrations completed successfully."
+    fi
+  '';
+
+  enterShell = ''
+    cargo --version
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # See full reference at https://devenv.sh/reference/options/
+}
